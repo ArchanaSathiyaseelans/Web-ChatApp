148d63dc0396d820249546949cacabc0a8acbb7e
ArchanaSathiyaseelans <archanasathiyaseelans26@gmail.com>
2026-06-26 00:25:17 +0530

    Add frrontend and connected backend and backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,29 @@
 import express from "express";
 import cors from "cors";
+
 import "dotenv/config";
+
 import fs from "fs";
 import path from "path";
+
 import { clerkMiddleware } from "@clerk/express";
+
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
-
-import dns from "dns";
 import job from "./lib/cron.js";
 
-dns.setServers(["0.0.0.0", "8.8.8.8"]);
+import clerkWebhook from "./webhooks/clerk.webhook.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
 const PORT = process.env.PORT;
-
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
 
 // it's important that you don't parse the webhook event data, it should be in the raw format
-app.use(
-  "/api/webhooks/clerk",
-  express.raw({ type: "application/json" }),
-  clerkWebhook,
-);
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
@@ -33,6 +32,9 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // if the public directory exists, serve the static files
 // this is for the production build
@@ -44,9 +46,9 @@ if (fs.existsSync(publicDir)) {
   });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDB();
-  console.log("server is up and running on PORT:", PORT);
+  console.log("Server is up and running on PORT:", PORT);
 
   if (process.env.NODE_ENV === "production") job.start();
 });
